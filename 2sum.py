45e2230a04f9b37fd9ef2dd0dce23f2825e9b5f3
Akash Santra <santraakash999@gmail.com>
2026-09-18 18:58:19 +0530

    add

--- 2sum.py
+++ 2sum.py
@@ -1,7 +1,6 @@
-arr = [1,2,3,4,5]
-sum = 9
-
+arr = [1,2,3,5,9,4]
+target = 9
 for i in range(len(arr)):
     for j in range(i+1,len(arr)):
-        if(arr[i]+arr[j]==sum):
+        if(arr[i]+arr[j]==target):
             print(i,j)
